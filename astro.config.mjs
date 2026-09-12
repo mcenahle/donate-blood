940c201642ba355d87781fe5534cba5e5e6c2e6c
mcenahle <hi@mcenahle.page>
2026-09-12 15:36:55 +0800

    refactor: 改为 bootstrap 3 样式
    还是这个好。哎。熟悉就行。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,5 +10,28 @@ export default defineConfig({
   prefetch: true,
   devToolbar: {
     enabled: false,
+  },
+  integrations: [icon()],
+
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+          rel: ["noopener", "noreferrer"],
+
+          test(element) {
+            const href = element.properties?.href;
+
+            return typeof href === "string" && /^https?:\/\//.test(href);
+          },
+
+          properties: {
+            className: ["external-link"],
+          },
+        },
+      ],
+    ],
   },
 });
